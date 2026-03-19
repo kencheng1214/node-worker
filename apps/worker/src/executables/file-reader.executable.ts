8da8e43681c75ee0e0a121d86cee1accba4eb766
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-19 17:26:10 +0800

    update

--- apps/worker/src/executables/file-reader.executable.ts
+++ apps/worker/src/executables/file-reader.executable.ts
@@ -1,13 +1,28 @@
 import { createReadStream } from 'node:fs';
 import { Injectable } from '@nestjs/common';
+import { parse } from 'csv';
 import split2 from 'split2';
 import { Executable, ExecutionContext } from '../app.interface';
 
 @Injectable()
 export class FileReader implements Executable {
-  execute(context: ExecutionContext, options?: Record<string, any>) {
+  execute(
+    context: ExecutionContext,
+    options?: {
+      parser?: 'line' | 'csv';
+    },
+  ) {
     if (!context.path) throw new Error();
 
-    context.readStream = createReadStream(context.path).pipe(split2());
+    const parser = (() => {
+      switch (options.parser) {
+        case 'csv':
+          return parse();
+        default:
+          return split2();
+      }
+    })();
+
+    context.readStream = createReadStream(context.path).pipe(parser);
   }
 }
