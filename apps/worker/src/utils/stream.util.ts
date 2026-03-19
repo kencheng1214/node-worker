7a70298691322f2d772a22f5a7682329ac764524
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-19 23:56:27 +0800

    update

--- apps/worker/src/utils/stream.util.ts
+++ apps/worker/src/utils/stream.util.ts
@@ -1,23 +1,23 @@
 import { EOL } from 'node:os';
-import { Duplex, Transform } from 'node:stream';
+import { Transform } from 'node:stream';
+import pumpify from 'pumpify';
 import split from 'split2';
 
-export function slice(options?: { head?: number; last?: number }) {
+export function trim(options?: { start?: number; end?: number }) {
   const EOL_BUFFER = Buffer.from(EOL);
+  const { start = 0, end = 0 } = options ?? {};
   const writable = split((string) => Buffer.from(string));
   const readable = Transform.from(async function* (lines: AsyncIterable<Buffer>) {
     const buffer: Buffer[] = [];
     let count = 0;
 
     for await (const line of lines) {
-      if (count++ < (options?.head ?? 0)) continue;
+      if (count++ < start) continue;
 
       buffer.push(line);
-      if (buffer.length > (options?.last ?? 0)) yield Buffer.concat([buffer.shift(), EOL_BUFFER]);
+      if (buffer.length > end) yield Buffer.concat([buffer.shift(), EOL_BUFFER]);
     }
   });
 
-  writable.pipe(readable);
-
-  return Duplex.from({ writable, readable });
+  return pumpify(writable, readable);
 }
