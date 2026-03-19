b4a431e1f40de29179a73f0301399458673d8781
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-19 20:00:54 +0800

    update

--- apps/worker/src/executables/file-writer.executable.ts
+++ apps/worker/src/executables/file-writer.executable.ts
@@ -1,6 +1,8 @@
 import { createWriteStream } from 'node:fs';
+import { Injectable } from '@nestjs/common';
 import { Executable, ExecutionContext } from '../app.interface';
 
+@Injectable()
 export class FileWriter implements Executable {
   execute(context: ExecutionContext, options: { path: string }) {
     if (!context.readStream) throw new Error();
