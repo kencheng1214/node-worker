d3ee12eb93167e445b256ef8135f467bfc17744c
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-19 13:50:28 +0800

    update

---
+++ apps/worker/src/plugins/file-poller.plugin.ts
@@ -0,0 +1,7 @@
+import { Executable, ExecutionContext } from '../app.interface';
+
+export class FilePoller implements Executable {
+  constructor(options?: {}) {}
+
+  execute(context: ExecutionContext) {}
+}
