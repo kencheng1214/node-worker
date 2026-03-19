e7e0c01c3776c90bfcd33d12b75a34125d33c04a
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-20 00:14:08 +0800

    update

--- apps/worker/src/utils/stream.util.ts
+++ apps/worker/src/utils/stream.util.ts
@@ -8,14 +8,14 @@ export function trim(options?: { start?: number; end?: number }) {
   const { start = 0, end = 0 } = options ?? {};
   const writable = split((string) => Buffer.from(string));
   const readable = Transform.from(async function* (lines: AsyncIterable<Buffer>) {
-    const buffer: Buffer[] = [];
-    let count = 0;
+    const window: Buffer[] = [];
+    let index = 0;
 
     for await (const line of lines) {
-      if (count++ < start) continue;
+      if (index++ < start) continue;
 
-      buffer.push(line);
-      if (buffer.length > end) yield Buffer.concat([buffer.shift(), EOL_BUFFER]);
+      window.push(line);
+      if (window.length > end) yield Buffer.concat([window.shift(), EOL_BUFFER]);
     }
   });
 
