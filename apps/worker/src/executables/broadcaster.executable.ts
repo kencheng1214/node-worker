08fcd6649fe459076fb34186bbe6355de1eb32e2
Ken Cheng <kencheng1214@gmail.com>
2026-03-21 01:41:27 +0800

    update

--- apps/worker/src/executables/broadcaster.executable.ts
+++ apps/worker/src/executables/broadcaster.executable.ts
@@ -4,8 +4,6 @@ import { BroadcasterOptions } from './broadcaster.schema';
 
 @Injectable()
 export class Broadcaster implements Executable {
-  readonly type = 'fan-out';
-
   execute(input: NodeJS.ReadableStream, options?: BroadcasterOptions) {
     throw new Error('Broadcaster is not implemented yet');
   }
