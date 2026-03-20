e4d150cb4cfbb8aa0a4170209d503f9d5be47e44
Ken Cheng <kencheng1214@gmail.com>
2026-03-21 00:06:16 +0800

    update

--- apps/worker/src/executables/stdout-writer.executable.ts
+++ apps/worker/src/executables/stdout-writer.executable.ts
@@ -1,3 +1,4 @@
+import { finished } from 'node:stream/promises';
 import { Injectable } from '@nestjs/common';
 import { Executable } from '../app.interface';
 import { StdoutWriterOptions } from './stdout-writer.schema';
@@ -5,10 +6,7 @@ import { StdoutWriterOptions } from './stdout-writer.schema';
 @Injectable()
 export class StdoutWriter implements Executable {
   async execute(input: NodeJS.ReadableStream, options?: StdoutWriterOptions) {
-    for await (const chunk of input) {
-      const canContinue = process.stdout.write(chunk);
-      if (canContinue) continue;
-      await new Promise<void>((resolve) => process.stdout.once('drain', resolve));
-    }
+    input.pipe(process.stdout, { end: false });
+    await finished(input);
   }
 }
