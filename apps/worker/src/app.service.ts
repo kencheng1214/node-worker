2466aea6e22bd24728431e4b7f7656c8c95e07b1
Ken Cheng <kencheng1214@gmail.com>
2026-03-20 23:35:29 +0800

    update

--- apps/worker/src/app.service.ts
+++ apps/worker/src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
   constructor(private readonly moduleRef: ModuleRef) {}
 
   async run(specification: Specification) {
-    let input: unknown = undefined;
+    let input = undefined;
 
     for (const step of specification.pipeline)
       input = await this.moduleRef.get<Executable>(step.name).execute(input, step.options);
