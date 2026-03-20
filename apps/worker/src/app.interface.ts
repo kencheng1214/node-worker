6738073359d1e6887651d7124a1385a25f6ad1c6
Ken Cheng <kencheng1214@gmail.com>
2026-03-20 23:36:15 +0800

    update

--- apps/worker/src/app.interface.ts
+++ apps/worker/src/app.interface.ts
@@ -1,5 +1,29 @@
 import { z } from 'zod';
-import { SpecificationSchema } from './app.constant';
+import { ArchiverSchema } from './executables/archiver.schema';
+import { BatcherSchema } from './executables/batcher.schema';
+import { CsvParserSchema } from './executables/csv-parser.schema';
+import { FileReaderSchema } from './executables/file-reader.schema';
+import { FileWriterSchema } from './executables/file-writer.schema';
+import { InspectorSchema } from './executables/inspector.schema';
+import { PackerSchema } from './executables/packer.schema';
+import { StringifierSchema } from './executables/stringifier.schema';
+import { TrimmerSchema } from './executables/trimmer.schema';
+
+export const SpecificationSchema = z.object({
+  pipeline: z.array(
+    z.discriminatedUnion('name', [
+      FileReaderSchema,
+      TrimmerSchema,
+      CsvParserSchema,
+      StringifierSchema,
+      BatcherSchema,
+      PackerSchema,
+      ArchiverSchema,
+      FileWriterSchema,
+      InspectorSchema,
+    ]),
+  ),
+});
 
 export type Specification = z.infer<typeof SpecificationSchema>;
 
