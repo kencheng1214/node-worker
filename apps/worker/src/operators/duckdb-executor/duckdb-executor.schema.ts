ffb422c3fbc377143bf3e44aa4d0978888aae11d
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-25 14:06:57 +0800

    update

--- apps/worker/src/operators/duckdb-executor/duckdb-executor.schema.ts
+++ apps/worker/src/operators/duckdb-executor/duckdb-executor.schema.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 
 export const DuckDBExecutorSchema = z.object({
   name: z.literal('DuckDBExecutor'),
-  options: z.object(),
+  options: z.object({
+    sql: z.string(),
+  }),
 });
 
 export type DuckDBExecutorOptions = z.infer<typeof DuckDBExecutorSchema>['options'];
--- apps/worker/src/operators/duckdb-executor/duckdb-executor.ts
+++ apps/worker/src/operators/duckdb-executor/duckdb-executor.ts
@@ -1,8 +1,18 @@
+import { Readable } from 'node:stream';
 import { Injectable } from '@nestjs/common';
+import { DuckDBInstance } from '@duckdb/node-api';
 import { Executable, PipelineContext } from '../../app.interface';
+import { compile } from '../../utils/compile';
 import { DuckDBExecutorOptions } from './duckdb-executor.schema';
 
 @Injectable()
 export class DuckDBExecutor implements Executable {
-  execute(input: NodeJS.ReadableStream, context: PipelineContext, options: DuckDBExecutorOptions) {}
+  async execute(input: NodeJS.ReadableStream, context: PipelineContext, options: DuckDBExecutorOptions) {
+    const templates = compile(options, 'sql');
+    const instance = await DuckDBInstance.create(':memory:');
+    const connection = await instance.connect();
+    const result = await connection.stream(context.render(templates.sql));
+
+    return Readable.from(result.yieldRows());
+  }
 }
