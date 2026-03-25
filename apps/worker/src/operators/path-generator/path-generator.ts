dbe65f36829e9838a26193bc3777ba65fc302c4c
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-25 10:57:59 +0800

    update

--- apps/worker/src/operators/path-generator/path-generator.ts
+++ apps/worker/src/operators/path-generator/path-generator.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { globStream } from 'glob';
 import { Executable, PipelineContext } from '../../app.interface';
+import { compile } from '../../utils/compile';
 import { PathGeneratorOptions } from './path-generator.schema';
 
 @Injectable()
 export class PathGenerator implements Executable {
   execute(input: unknown, context: PipelineContext, options: PathGeneratorOptions) {
-    return globStream(options.pattern);
+    const patterns = (Array.isArray(options.pattern) ? options.pattern : [options.pattern]).map((pattern) =>
+      context.render(compile(pattern)),
+    );
+
+    return globStream(patterns);
   }
 }
--- apps/worker/src/utils/compile.ts
+++ apps/worker/src/utils/compile.ts
@@ -1,8 +1,17 @@
 import dotProp from 'dot-prop';
 import Handlebars from 'handlebars';
 
-export function compile(options: Record<string, unknown>, ...paths: string[]) {
-  if (!options) return {};
+export function compile(input: string): ReturnType<typeof Handlebars.compile>;
+export function compile(
+  options: Record<string, unknown>,
+  ...paths: string[]
+): Record<string, ReturnType<typeof Handlebars.compile>>;
+export function compile(
+  inputOrOptions: string | Record<string, unknown>,
+  ...paths: string[]
+): ReturnType<typeof Handlebars.compile> | Record<string, ReturnType<typeof Handlebars.compile>> {
+  if (typeof inputOrOptions === 'string') return Handlebars.compile(inputOrOptions);
+  if (!inputOrOptions) return {};
 
-  return Object.fromEntries(paths.map((path) => [path, Handlebars.compile(dotProp.get(options, path))]));
+  return Object.fromEntries(paths.map((path) => [path, Handlebars.compile(dotProp.get(inputOrOptions, path))]));
 }
