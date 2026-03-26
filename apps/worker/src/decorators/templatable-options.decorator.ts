0340106de33bb6908d3b11c56c0693bbe0e3b137
Cheng, Ken MK (IT) <02005424@corphq.hk.pccw.com>
2026-03-26 12:29:39 +0800

    update

--- apps/worker/src/decorators/templatable-options.decorator.ts
+++ apps/worker/src/decorators/templatable-options.decorator.ts
@@ -1,7 +1,7 @@
-export const TEMPLATABLE_OPTIONS_METADATA = 'TEMPLATABLE_OPTIONS_METADATA';
+export const TEMPLATABLE_OPTIONS_PATHS = 'TEMPLATABLE_OPTIONS_PATHS';
 
 export const TemplatableOptions: (...paths: string[]) => ClassDecorator = (...paths: string[]) => {
   return (target: object) => {
-    Reflect.defineMetadata(TEMPLATABLE_OPTIONS_METADATA, paths, target);
+    Reflect.defineMetadata(TEMPLATABLE_OPTIONS_PATHS, paths, target);
   };
 };
--- apps/worker/src/services/pipeline.service.ts
+++ apps/worker/src/services/pipeline.service.ts
@@ -1,7 +1,7 @@
 import { ModuleRef } from '@nestjs/core';
 import { Injectable } from '@nestjs/common';
 import { Executable, PipelineContext, Specification } from '../app.interface';
-import { TEMPLATABLE_OPTIONS_METADATA } from '../decorators/templatable-options.decorator';
+import { TEMPLATABLE_OPTIONS_PATHS } from '../decorators/templatable-options.decorator';
 import { compile2 } from '../utils/compile';
 
 @Injectable()
@@ -11,7 +11,7 @@ export class PipelineService {
   async runPipeline(pipeline: Specification['pipeline'], input = undefined, context: PipelineContext) {
     for (const operator of pipeline) {
       const executable = this.moduleRef.get<Executable>(operator.name);
-      const paths = Reflect.getMetadata(TEMPLATABLE_OPTIONS_METADATA, executable.constructor);
+      const paths = Reflect.getMetadata(TEMPLATABLE_OPTIONS_PATHS, executable.constructor);
 
       if (paths) compile2(context, operator.options, ...paths);
       input = await executable.execute(input, context, operator.options);
